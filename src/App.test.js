27e4eb004b058d4f61b7e58dde2ce99b2b69bd6d
felipe_c_rodrigues <felipe.cavalcante.rod@gmail.com>
2020-09-09 23:18:51 -0300

    adicionado projeto ao firebase, adicionado novo teste e corrigido teste existente

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,15 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/My Finances/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+function mult(a, b) {
+  return a * b;
+}
+
+test('Teste de multiplicacao', () => {
+  expect(mult(3, 2)).toBe(6);
+})
